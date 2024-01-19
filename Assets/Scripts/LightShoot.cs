6fed318409d659c6330fcac36a488facaebfadeb
Andy Wang <andy_wang02@hotmail.com>
2024-01-19 14:27:05 -0500

    added cooldown to firing

--- Assets/Scripts/LightShoot.cs
+++ Assets/Scripts/LightShoot.cs
@@ -7,17 +7,27 @@ using UnityEngine;
 public class LightShoot : MonoBehaviour
 {
     public GameObject lightProjectile;  // projectile to shoot
+    public float cooldown = 1;
     public float lightSpeed = 50;  // speed of projectile
+
+    private float timer = 0;
+
+    void Start() {
+        timer = cooldown;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && (timer >= cooldown))
         {
+            timer = 0;
             Vector3 direction = transform.up;
 
             GameObject newLight = Instantiate(lightProjectile, transform.position, Quaternion.identity);
             newLight.GetComponent<Rigidbody2D>().velocity = direction * lightSpeed;
         }
+
+        timer += Time.deltaTime;
     }
 }
