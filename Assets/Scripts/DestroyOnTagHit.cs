11ab54fd56be024623ca1d1312d08cac6a9e88ad
Andy Wang <andy_wang02@hotmail.com>
2024-01-19 14:19:18 -0500

    adjusted light being destroyed when it doesn't hit a mirror

--- Assets/Scripts/DestroyOnTagHit.cs
+++ Assets/Scripts/DestroyOnTagHit.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Projectiles that are destroyed when hitting ground
-public class DestroyOnGroundHit : MonoBehaviour
+public class DestroyOnNotTagHit : MonoBehaviour
 {
+    public string targetTag;
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.collider.gameObject.tag == "Ground") {
+        if (col.collider.gameObject.tag != targetTag) {
             Destroy(gameObject);
         }
     }
