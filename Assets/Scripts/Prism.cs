ba81cf7788f577ea6f9466a01e7ab777c89604ac
Andy Wang <andy_wang02@hotmail.com>
2024-01-20 16:04:55 -0500

    added prism and lvl 7

---
+++ Assets/Scripts/Prism.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Detects a light projectile, destroys it, and spawns two more at a 90 angle
+public class Prism : MonoBehaviour
+{
+    public float angle = 90;
+
+    private float cooldown = 0.8f;  // cooldown to prevent split light from triggering the prism
+    private float timer = 0.8f;
+
+    void Update() {
+        timer += Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag != "Light" || timer < cooldown) {
+            return;
+        }
+
+        timer = 0;
+
+        // Must be tagged Light to split
+        GameObject light = other.gameObject;
+        Vector3 velo = light.GetComponent<Rigidbody2D>().velocity;
+        float currSpeed = velo.magnitude;
+        Vector3 dir = velo.normalized;
+        Vector3 dirUp = Quaternion.AngleAxis(angle / 2, new Vector3(0, 0, 1)) * dir;
+        Vector3 dirDown = Quaternion.AngleAxis(-angle / 2, new Vector3(0, 0, 1)) * dir;
+
+        LightShoot.Shoot(light, light.transform.position, dirUp, currSpeed);
+        LightShoot.Shoot(light, light.transform.position, dirDown, currSpeed);
+
+        Destroy(light);
+    }
+}
