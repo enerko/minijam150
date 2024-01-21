4e424aebd20b42778fadc4dff006bdeace503ddf
Andy Wang <andy_wang02@hotmail.com>
2024-01-21 11:09:10 -0500

    fixed bug with light particles

--- Assets/Scripts/DestroyOnNotTagHit.cs
+++ Assets/Scripts/DestroyOnNotTagHit.cs
@@ -7,9 +7,14 @@ public class DestroyOnNotTagHit : MonoBehaviour
 {
     public string targetTag;
     public AudioClip sound;  // sound to play when hitting target thing
+    public ParticleSystem particles;
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        if (particles) {
+            Instantiate(particles, transform.position, Quaternion.identity);
+        }
+
         if (col.collider.gameObject.tag != targetTag) {
             Destroy(gameObject);
         } else {
